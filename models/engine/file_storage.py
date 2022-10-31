5514cfbf0cdbcb1c9e7f85c1845078d1a00a8f81
Charles Obinna Iro <irocharles22@gmail.com>
2022-10-30 20:23:40 -0700

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,12 @@
 "define a FileStorage class"
 import json
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
 import re
 from os.path import exists, getsize
 
@@ -33,7 +39,8 @@ class FileStorage:
                     if FileStorage.__objects:
                         dic_t = {}
                         for i, j in FileStorage.__objects.items():
-                            dic_t[i] = j.to_dict()
+                            if isinstance(j, BaseModel):
+                                dic_t[i] = j.to_dict()
                         json.dump(dic_t, myfile)
 
     def reload(self):
