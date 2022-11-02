4713a7afd5dca12e55c35cf80869267eedd73c1d
Charles Obinna Iro <irocharles22@gmail.com>
2022-11-02 04:08:53 -0700

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 "define a class BaseModel"
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -9,29 +10,23 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         "initializing the attributes required"
-        from models import storage
+
+        iso_format = '%Y-%m-%dT%H:%M:%S.%f'
         if kwargs:
             for i, j in kwargs.items():
-                if i == 'id':
-                    self.id = j
-                elif i == 'created_at':
-                    self.created_at = datetime.strptime(j,
-                                                        '%Y-%m-%dT%H:%M:%S.%f')
-                elif i == 'updated_at':
-                    self.updated_at = datetime.strptime(j,
-                                                        '%Y-%m-%dT%H:%M:%S.%f')
+                if i in ['created_at', 'updated_at']:
+                    self.__dict__[i] = datetime.strptime(j, iso_format)
                 elif i != '__class__':
                     self.__dict__[i] = j
         else:
             self.id = uuid.uuid4().hex
             self.created_at = self.updated_at = datetime.now()
-        storage.new(self)
+        models.storage.new(self)
 
     def save(self):
-        "update the attribute 'updated_at' "
-        from models import storage
+        "update the attribute 'updated_at'"
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def __str__(self):
         return "[{0}] ({1}) {2}".format(self.__class__.__name__,
